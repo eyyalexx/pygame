2ee82266001624a174bd76914e0e5837ffdec2d0
Alex Grigorev <alex.grigorev@hotmail.com>
2021-01-21 15:58:47 -0500

    added pipes, and pipe movement

--- game.py
+++ game.py
@@ -1,8 +1,28 @@
-import pygame, sys
+import pygame, sys, random
 
 def draw_floor():
-        screen.blit(floor_surface, (floor_x_pos,900))
-        screen.blit(floor_surface, (floor_x_pos + 576, 900))
+    screen.blit(floor_surface, (floor_x_pos,900))
+    screen.blit(floor_surface, (floor_x_pos + 576, 900))
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop = (700,random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom = (700,random_pipe_pos - 300))
+    return bottom_pipe, top_pipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 1024:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+            screen.blit(flip_pipe, pipe)
+
 
 
 pygame.init()
@@ -28,6 +48,13 @@ bird_surface = pygame.image.load('assets/sprites/bluebird-midflap.png').convert(
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (100, 512))
 
+pipe_surface = pygame.image.load('assets/sprites/pipe-green.png').convert()
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1200)
+pipe_height = [400, 600, 800]
+
 while True:
 
     #exit game
@@ -40,14 +67,23 @@ while True:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 12
+        
+        if event.type == SPAWNPIPE:
+            pipe_list.extend(create_pipe())
+            print(pipe_list)
 
     screen.blit(bg_surface, (0,0))
 
+    # Bird
     bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird_surface, bird_rect)
 
+    # Pipes
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
 
+    # Floor
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -576:
