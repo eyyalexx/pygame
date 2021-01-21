f1ac4e0e9e013bb1d5b8c5dd25d3af14252d8c36
Alex Grigorev <alex.grigorev@hotmail.com>
2021-01-21 16:14:06 -0500

    added pipe collisions

--- game.py
+++ game.py
@@ -23,6 +23,15 @@ def draw_pipes(pipes):
             flip_pipe = pygame.transform.flip(pipe_surface, False, True)
             screen.blit(flip_pipe, pipe)
 
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        return False
+    
+    return True
 
 
 pygame.init()
@@ -36,6 +45,7 @@ clock = pygame.time.Clock()
 #Game Variables
 gravity = 0.25
 bird_movement = 0
+game_active = True
 
 bg_surface = pygame.image.load('assets/sprites/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -74,14 +84,16 @@ while True:
 
     screen.blit(bg_surface, (0,0))
 
-    # Bird
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird_surface, bird_rect)
+    if game_active:
+        # Bird
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface, bird_rect)
+        game_active = check_collision(pipe_list)
 
-    # Pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        # Pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
     # Floor
     floor_x_pos -= 1
