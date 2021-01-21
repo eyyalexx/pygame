5007dadde87510f127055bc31389f9b4a44e81e1
Alex Grigorev <alex.grigorev@hotmail.com>
2021-01-21 15:15:09 -0500

    added assets, and moving floor

--- game.py
+++ game.py
@@ -1,7 +1,24 @@
 import pygame, sys
 
+def draw_floor():
+        screen.blit(floor_surface, (floor_x_pos,900))
+        screen.blit(floor_surface, (floor_x_pos + 576, 900))
+
+
 pygame.init()
+
+#create window
 screen = pygame.display.set_mode((576,1024))
+
+#clock for frame rate
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('assets/sprites/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load('assets/sprites/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
 
 while True:
 
@@ -11,5 +28,20 @@ while True:
             pygame.quit()
             sys.exit()
 
-    pygame.display.update()
 
+    screen.blit(bg_surface, (0,0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
+
+
+
+
+
+
+
+    pygame.display.update()
+    #frame rate
+    clock.tick(120)
